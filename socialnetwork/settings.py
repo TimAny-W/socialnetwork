bab9f2bbf4b6181e73048ad96c15d5de60f7a8d0
TimAnys <109108903+TimAnys@users.noreply.github.com>
2022-10-14 00:07:35 +0300

    a bit of front end made
    .

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -133,6 +133,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = "http://127.0.0.1:8000//media/"
 
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
